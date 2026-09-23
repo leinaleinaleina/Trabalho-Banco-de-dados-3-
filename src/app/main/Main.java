59f6484f4b81b31d2772bcd76c261e5e1801616e
thay_jpeg <yuukellyndj@gmail.com>
2026-09-23 16:15:16 -0300

    updanting models of studycase. fixing base arquitecture, adding controller and main (not finished)

--- src/app/main/Main.java
+++ src/app/main/Main.java
@@ -1,5 +1,26 @@
 package app.main;
 
+import app.main.controller.SimulacaoController;
+import java.util.Scanner;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        SimulacaoController controller = new SimulacaoController();
+        
+        try {
+            //--------> FALTA inicializar a conexão com o SGBD (JDBC)
+            System.out.println("Conectando ao banco de dados...");
+            
+            // inicia a interface
+            controller.iniciar(scanner);
+            
+        } catch (Exception e) {
+            System.out.println("Erro crítico na simulação: " + e.getMessage());
+        } finally {
+            scanner.close();
+
+            //-------->FALTA fechar a conexão com o banco
+        }
+    }
 }
